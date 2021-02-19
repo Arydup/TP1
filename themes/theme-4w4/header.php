dc6310d04c0aa4cedcb31cf041d5def441fe6825
Aryane Duperron <arydup@hotmail.com>
2021-02-19 09:59:01 -0500

    commit original

--- themes/theme-4w4/header.php
+++ themes/theme-4w4/header.php
@@ -46,7 +46,8 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'theme-4w4' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><img src="https://s2.svgbox.net/materialui.svg?ic=menu&color=000" width="32" height="32"></button>
+			<!-- ?php esc_html_e( 'Primary Menu', 'theme-4w4' ); ?>-->
 			<?php
 			wp_nav_menu(
 				array(
